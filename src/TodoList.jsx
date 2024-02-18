59d2c77a12e7c31c03fd1b83689ea4729fbca050
JennyMyy <jenny.mattila@elisanet.fi>
2024-02-18 11:43:11 +0200

    Todolist

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -2,29 +2,55 @@ import { useState } from "react";
 
 function TodoList() {
 
-    const [desc, setDesc] = useState("");
+    const [desc, setDesc] = useState({ description: '', date: '' });
     const [todos, setTodos] = useState([]);
 
-    const handleChange = (event) => {
-        setDesc(event.target.value);
-    };
+    const handleChange = (event) => 
+        setDesc({ ...desc, [event.target.name]: event.target.value });
+    ;
 
     const addTodo = () => {
         setTodos([...todos, desc]);
+        console.log(todos);
+        setDesc({ description: '', date: '' })
     };
+
+    const itemsRows = todos.map((desc, index) =>
+        <tr key={index}>
+            <td>{desc.date}</td>
+            <td>{desc.description}</td>
+            <td><button onClick={() => deleteByIndex(index)}>Delete</button></td>
+        </tr>
+    )
+
+    const deleteByIndex = (index) => {
+        setTodos(todos.filter((desc, i) => i !== index));
+    }
 
     return (
         <>
-            <input placeholder="Description" onChange={handleChange} value={desc} />
+            <h3>Add todo:</h3>
+            Description: <input
+                type="text"
+                name="description"
+                value={desc.description}
+                onChange={handleChange}
+            />
+            Date: <input
+                type="text"
+                name="date"
+                value={desc.date}
+                onChange={handleChange}
+            />
             <button onClick={addTodo}>Add</button>
             <table>
-                <tbody>
-                    {todos.map((todo, index) => (
-                        <tr key={index}>
-                            <td>{todo}</td>
-                        </tr>
-                    ))}
-                </tbody>
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Description</th>
+                    </tr>
+                    {itemsRows}
+                </thead>
             </table>
         </>
     );
